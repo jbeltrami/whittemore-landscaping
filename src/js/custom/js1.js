2ea5e2bfd2b4de768ed3115264f77c1ba49061a5
jbeltrami <joao_beltrami@hotmail.com>
2019-02-26 20:20:52 -0500

    Add colors and fix Typography.

--- src/js/custom/js1.js
+++ src/js/custom/js1.js
@@ -2,10 +2,13 @@ $(() => {
   $('.owl-carousel').each(function(i, e) {
     $(e).owlCarousel({
       margin: 10,
+      center: true,
       loop: true,
       autoWidth: true,
       items: 4,
       nav: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
     });
   });
 });
